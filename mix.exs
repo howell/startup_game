d7569c6c10db67fc503a5b7bddead9e204c40551
Sam Caldwell <samcaldwell19@gmail.com>
2025-03-18 16:45:06 -0400

    add dialyzer options to mix.exs

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule StartupGame.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
