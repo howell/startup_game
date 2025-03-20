eca8f3c1034fded025b42a9ec46e15765a83cb40
Sam Caldwell <samcaldwell19@gmail.com>
2025-03-20 16:13:22 -0400

    run app config in mix cli task

--- lib/mix/tasks/game/cli.ex
+++ lib/mix/tasks/game/cli.ex
@@ -14,6 +14,7 @@ defmodule Mix.Tasks.Game.Cli do
   @spec run(list()) :: :ok
   def run(_) do
     # Ensure all dependencies are started
+    Mix.Task.run("app.config")
     Application.ensure_all_started(:startup_game)
     StartupGame.CLI.Main.start()
   end
