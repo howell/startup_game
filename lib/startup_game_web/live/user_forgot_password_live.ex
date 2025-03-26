2e66a1525f1d9fd771b0a08454384fdc9ea67d48
Sam Caldwell <samcaldwell19@gmail.com>
2025-03-26 11:57:31 -0400

    revise forgot password page style

--- lib/startup_game_web/live/user_forgot_password_live.ex
+++ lib/startup_game_web/live/user_forgot_password_live.ex
@@ -1,34 +1,69 @@
 defmodule StartupGameWeb.UserForgotPasswordLive do
   use StartupGameWeb, :live_view
+  import StartupGameWeb.CoreComponents
 
   alias StartupGame.Accounts
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Forgot your password?
-        <:subtitle>We'll send a password reset link to your inbox</:subtitle>
-      </.header>
+    <div class="flex-1 flex items-center justify-center p-4">
+      <div class="w-full max-w-md bg-white rounded-lg shadow-md p-6">
+        <div class="text-center mb-6">
+          <h1 class="text-2xl font-bold">Reset password</h1>
+          <p class="text-gray-600 mt-1">
+            Enter your email address and we'll send you a link to reset your password
+          </p>
+        </div>
 
-      <.simple_form for={@form} id="reset_password_form" phx-submit="send_email">
-        <.input field={@form[:email]} type="email" placeholder="Email" required />
-        <:actions>
-          <.button phx-disable-with="Sending..." class="w-full">
-            Send password reset instructions
-          </.button>
-        </:actions>
-      </.simple_form>
-      <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
-      </p>
+        <.simple_form
+          for={@form}
+          id="reset_password_form"
+          phx-submit="send_email"
+          phx-change="validate"
+          class="space-y-4"
+        >
+          <.input
+            field={@form[:email]}
+            type="email"
+            label="Email"
+            placeholder="you@example.com"
+            autocomplete="email"
+            phx-debounce="300"
+            required
+            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-silly-blue focus:border-silly-blue"
+          />
+
+          <:actions>
+            <.button
+              phx-disable-with="Sending reset link..."
+              class="w-full bg-silly-blue text-white font-medium py-2 px-4 rounded-md hover:bg-silly-blue/90 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-silly-blue"
+            >
+              Send reset link
+            </.button>
+          </:actions>
+        </.simple_form>
+
+        <div class="mt-6 text-center">
+          <.link navigate={~p"/users/log_in"} class="text-silly-blue hover:underline">
+            Back to log in
+          </.link>
+        </div>
+      </div>
     </div>
     """
   end
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket, form: to_form(%{}, as: "user"))}
+  end
+
+  def handle_event("validate", %{"user" => %{"email" => email}}, socket) do
+    form =
+      %{"email" => email}
+      |> validate_email()
+      |> to_form(as: "user")
+
+    {:noreply, assign(socket, form: form)}
   end
 
   def handle_event("send_email", %{"user" => %{"email" => email}}, socket) do
@@ -46,5 +81,14 @@ defmodule StartupGameWeb.UserForgotPasswordLive do
      socket
      |> put_flash(:info, info)
      |> redirect(to: ~p"/")}
+  end
+
+  defp validate_email(params) do
+    types = %{email: :string}
+
+    {%{}, types}
+    |> Ecto.Changeset.cast(params, Map.keys(types))
+    |> Ecto.Changeset.validate_required([:email], message: "is required")
+    |> Ecto.Changeset.validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must be a valid email address")
   end
 end
