26e147aff9af9157498d1341e1eab03711d0532b
Sam Caldwell <samcaldwell19@gmail.com>
2025-04-09 21:35:42 -0400

    convert char list to string

--- lib/startup_game/release.ex
+++ lib/startup_game/release.ex
@@ -25,7 +25,7 @@ defmodule StartupGame.Release do
 
   def set_admin_role(email) do
     load_app()
-    Mix.Tasks.Users.SetRole.set_user_role(email, "admin")
+    Mix.Tasks.Users.SetRole.set_user_role(to_string(email), "admin")
   end
 
   @doc """
